d9d96a73d1f0355080e4415fb0c8be90d51a287b
MIchael <michael.olson756@gmail.com>
2016-09-20 13:31:03 -0600

    vecotors have been included

--- public/mail/contact_form.php
+++ public/mail/contact_form.php
@@ -5,7 +5,7 @@
 	$headers = "MIME-Version: 1.0\r\n";
 	$headers .= "Content-type: text/html; charset=utf-8\r\n";
 
-	$subject = "Contact form message"; // Enter your subject here
+	$subject = "Portfolio Site Response"; // Enter your subject here
 	$body = "";
 
 
